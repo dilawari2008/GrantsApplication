4e22ec433e447ec9a7effab923639768d1a70605
vagish@fibr.ai <vagish@fibr.ai>
2024-06-26 22:14:05 +0530

    Initial Commit

---
+++ src/main/java/com/example/grantsmanagement/GrantsManagement/GrantsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.grantsmanagement.GrantsManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrantsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrantsManagementApplication.class, args);
+	}
+
+}
