be40068f717407c68f6d92f43182b774bdb6b2cb
vagish@fibr.ai <vagish@fibr.ai>
2024-06-27 23:36:24 +0530

    Folder restructure

--- src/main/java/com/example/grantsmanagement/GrantsManagement/repositories/FoundationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.grantsmanagement.GrantsManagement.repositories;
-
-import com.example.grantsmanagement.GrantsManagement.models.Foundation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FoundationRepository extends JpaRepository<Foundation, Long> {
-}
